418f29bbf393b5fd8534880aa73c314f76fc2ce4
9hng4ybss9-rgb <9hng4ybss9@privaterelay.appleid.com>
2026-02-08 21:16:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function giveChocolate(e) {
   c.classList.remove("move-chocolate");
   void c.offsetWidth;
   c.classList.add("move-chocolate");
-  document.getElementById("choco-text").style.display = "block";
+  document.getElementById("chocoText").style.display = "block";
 }
 
 function giveTeddy(e) {
@@ -13,7 +13,7 @@ function giveTeddy(e) {
   t.classList.remove("teddy-bounce");
   void t.offsetWidth;
   t.classList.add("teddy-bounce");
-  document.getElementById("teddy-text").style.display = "block";
+  document.getElementById("teddyText").style.display = "block";
 }
 
 const promises = [
@@ -21,22 +21,22 @@ const promises = [
   "I’ll learn.",
   "I’ll stay."
 ];
-let promiseIndex = 0;
+let pIndex = 0;
 
 function nextPromise(e) {
   e.stopPropagation();
-  if (promiseIndex < promises.length) {
-    document.getElementById("promise").innerText += promises[promiseIndex++] + " ";
+  if (pIndex < promises.length) {
+    document.getElementById("promiseText").innerText += promises[pIndex++] + " ";
   }
 }
 
 function hug(e) {
   e.stopPropagation();
-  document.getElementById("hug-him").classList.add("hug-close");
-  document.getElementById("hug-her").classList.add("hug-close");
+  document.getElementById("hugHim").classList.add("hug-left");
+  document.getElementById("hugHer").classList.add("hug-right");
 }
 
 function anniversary(e) {
   e.stopPropagation();
-  document.getElementById("anniv-text").style.display = "block";
+  document.getElementById("annivText").style.display = "block";
 }
