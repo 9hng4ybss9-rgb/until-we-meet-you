15d7d44d83d8afb1b07bc24b4242ee6d93029616
9hng4ybss9-rgb <9hng4ybss9@privaterelay.appleid.com>
2026-02-08 18:58:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function scrollToContent() {
+  document.getElementById("content").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+const today = new Date();
+today.setHours(0, 0, 0, 0);
+
+const sections = document.querySelectorAll(".day");
+
+sections.forEach(section => {
+  const unlockDate = new Date(section.dataset.date);
+  unlockDate.setHours(0, 0, 0, 0);
+
+  if (today >= unlockDate) {
+    section.style.display = "block";
+  }
+});
